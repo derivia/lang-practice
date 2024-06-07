6c09a0898c9e79b76085d0f601e922ca1b51ed86
derivia <contato.lorransoares@gmail.com>
2024-06-07 13:27:44 -0300

    feat: adding httpserver example in java

---
+++ java/HTTPServer.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+class HTTPServer {
+  public static void main(String[] args) throws IOException {
+    ServerSocket httpServer = new ServerSocket(8080);
+    while (true) {
+      try (Socket socket = httpServer.accept()) {
+        Date currentDateAndTime = new Date();
+        String httpResponse = "HTTP/1.1 200 OK\r\n" +
+                              "Content-Type: text/plain\r\n" +
+                              "Content-Length: " + currentDateAndTime.toString().length() + "\r\n" +
+                              "\r\n" +
+                              currentDateAndTime;
+        socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+      }
+    }
+  }
+}
