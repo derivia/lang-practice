b85d28d152e941168fc1983bfee85d6d6da15b9e
derivia <contato.lorransoares@gmail.com>
2024-06-08 07:22:19 -0300

    refactor: now trying to send a proper HTTP response using Status and Content-Type

--- c/http_server.c
+++ c/http_server.c
@@ -17,6 +17,19 @@
  */
 // clang-format on
 
+void sendHTTPHeaders(int clientFileDescriptor, const char *status,
+                     const char *content_type, off_t content_length) {
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer),
+           "HTTP/1.0 %s\r\n"
+           "Content-Type: %s\r\n"
+           "Content-Length: %ld\r\n"
+           "Connection: keep-alive\r\n"
+           "\r\n",
+           status, content_type, content_length);
+  send(clientFileDescriptor, buffer, strlen(buffer), 0);
+}
+
 int main(int argc, char *argv[]) {
   // AF_INET expands to 2 & SOCK_STREAM expands to 1
   int socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,8 +38,9 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   // htons converts the port to network byte order
-  // htonl does the same for the IP address, which in this case is INADDR_LOOPBACK (localhost/127.0.0.1 in IPv4)
-  // sockaddr_in is a structure that holds an Internet socket address
+  // htonl does the same for the IP address, which in this case is
+  // INADDR_LOOPBACK (localhost/127.0.0.1 in IPv4) sockaddr_in is a structure
+  // that holds an Internet socket address
   struct sockaddr_in addr = {AF_INET, htons(8080), htonl(INADDR_LOOPBACK)};
   // clang-format off
   // binds the socket to the specific address:port combination
@@ -39,7 +53,8 @@ int main(int argc, char *argv[]) {
   }
   // makes the socket listen on the bound port
   listen(socketFileDescriptor, 32);
-  // infinite loop (til program quits) to accept new connections, creating new sockets
+  // infinite loop (til program quits) to accept new connections, creating new
+  // sockets
   while (1) {
     int clientFileDescriptor = accept(socketFileDescriptor, 0, 0);
     if (clientFileDescriptor == -1) {
@@ -57,28 +72,41 @@ int main(int argc, char *argv[]) {
       continue;
     }
     // gets the first token of buffer, which should have the GET HTTP method
-    char *fileName = strtok(buffer, " \t\n");
-    if (strcmp(fileName, "GET") != 0) {
+    char *method = strtok(buffer, " \t\n");
+    // gets the second token of buffer, which should be the file name
+    char *fileName = strtok(NULL, " \t\n");
+    if (strcmp(method, "GET") != 0) {
       perror("invalid HTTP request");
       close(clientFileDescriptor);
       continue;
     }
-    // gets the second token of buffer, which should be the file name
-    fileName = strtok(NULL, " \t\n");
+    if (strcmp(fileName, "/") == 0) {
+      fileName = "/index.html";
+    }
     char filePath[1024] = {0};
     // writes the file path to filePath, appending misc/
-    snprintf(filePath, sizeof(filePath), "misc/%s", fileName);
+    snprintf(filePath, sizeof(filePath), "misc%s", fileName);
     // opens the file in read only mode
     int openedFileDescriptor = open(filePath, O_RDONLY);
     if (openedFileDescriptor == -1) {
+      // sends an error stating that the file doesn't exist and closes the
+      // connection
+      const char *notFound =
+          "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n";
+      send(clientFileDescriptor, notFound, strlen(notFound), 0);
+      printf("file name received: %s", fileName);
+      printf("file path received: %s", filePath);
       perror("open failed");
       continue;
     }
-    off_t offset = 0;
     struct stat fileStat;
-    // fstat is used to retrieve information from the file associated with the specified file descriptor and store to an variable
+    // fstat is used to retrieve information from the file associated with the
+    // specified file descriptor and store to an variable
     fstat(openedFileDescriptor, &fileStat);
     // clang-format off
+    // sends the OK status code with content type
+    sendHTTPHeaders(clientFileDescriptor, "200 OK", "text/html", fileStat.st_size);
+    off_t offset = 0;
     // sends (copies) the content of the opened file (openedFileDescriptor) to clientFileDescriptor, which is the connection that is waiting on the socket that accepted it
     // the first argument should be an write mode file descriptor
     // and the second should be an read mode file descriptor
